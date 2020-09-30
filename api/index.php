51d1fabee498cacb67478f4f4e19180aca2a4634
Leo <54992384+frankeu@users.noreply.github.com>
2020-09-30 16:01:44 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,18 @@
 <?php
-
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.btpn.com/en/prime-lending-rate/kurs');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$page = preg_replace('/\r|\n/','',curl_exec($ch));
+curl_close($ch);
+preg_match_all('/<td>(.*?) \/ (.*?)<\/td>                                        <td>(.*?)<\/td>	                       				<td>(.*?)<\/td>/',$page,$data);
+foreach($data[1] as $k => $v){
+	if($data[2][$k] != 'IDR'){ continue; }
+	$rate[] = array(
+		'currency' => $data[1][$k],
+		'buy' => ceil(preg_replace('/,/','',$data[3][$k])),
+		'sell' => ceil(preg_replace('/,/','',$data[4][$k])),
+	);
+}
+header("Content-Type: application/json; charset=UTF-8");
+echo json_encode($rate,JSON_PRETTY_PRINT);
+?>
